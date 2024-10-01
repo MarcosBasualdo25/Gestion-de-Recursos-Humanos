f02ce12ea97fa251473d27095707fd4bf94a6220
Gabriel Poma <gabrielpoma21@gmail.com>
2024-10-01 16:10:14 -0500

    Agregue gotoxy, funcionalidad de evaluaciones por empleado usando pilas, modificacion para que la consola se limpie continuamente y no crashee, y un tipo de consola para que no haya problema con las tildes

--- GestionRecursosHumanos.cpp
+++ GestionRecursosHumanos.cpp
@@ -1,9 +1,127 @@
 #include <iostream>
-#include <windows.h>
+#include "GestionEmpleados.h"
+#include "EvaluacionEmpleado.h"
+#include "gotoxy.h"
 
 using namespace std;
 
-int main(){
-    cout<<">>>>>rarazo pes<<<<<<<<"<<endl;
-    system("pause");
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    Empleado* listaEmpleados = nullptr;
+    int opcionPrincipal;
+
+    do {
+        system("cls");
+        int anchoConsola = obtenerAnchoConsola();
+        int x = anchoConsola / 2 - 20;
+
+        gotoxy(x, 2);
+        cout << "--- Menú Principal ---\n";
+        gotoxy(x, 4);
+        cout << "1. Gestión de empleados\n";
+        gotoxy(x, 5);
+        cout << "2. Evaluacion de empleados\n";
+        gotoxy(x, 6);
+        cout << "3. Salir\n";
+        gotoxy(x, 8);
+        cout << "Ingrese una opción: ";
+        cin >> opcionPrincipal;
+
+        switch (opcionPrincipal) {
+            case 1: {
+                int continuar;
+                do {
+                    continuar = mostrarMenuGestionEmpleados(listaEmpleados);
+                } while (continuar != 0);
+                break;
+            }
+            case 2: {
+                if (listaEmpleados == nullptr) {
+                    gotoxy(x, 10);
+                    cout << "No hay empleados registrados. Regrese al menú y agregue empleados primero.\n";
+                    system("pause");
+                    break;
+                }
+                int id;
+                gotoxy(x, 10);
+                cout << "Ingrese el ID del empleado para evaluar: ";
+                cin >> id;
+
+                Empleado* empleado = buscarEmpleado(listaEmpleados, id);
+                if (empleado == nullptr) {
+                    gotoxy(x, 12);
+                    cout << "Empleado con ID " << id << " no encontrado.\n";
+                    system("pause");
+                    break;
+                }
+
+                int opcionEvaluacion;
+                do {
+                    system("cls");
+                    gotoxy(x, 2);
+                    cout << "--- Menú de Evaluación de Empleados ---\n";
+                    gotoxy(x, 4);
+                    cout << "Empleado: " << empleado->nombre << " " << empleado->apellido << "\n";
+                    gotoxy(x, 6);
+                    cout << "1. Agregar evaluación\n";
+                    gotoxy(x, 7);
+                    cout << "2. Mostrar evaluaciones\n";
+                    gotoxy(x, 8);
+                    cout << "3. Volver al menú principal\n";
+                    gotoxy(x, 10);
+                    cout << "Ingrese una opción: ";
+                    cin >> opcionEvaluacion;
+
+                    switch (opcionEvaluacion) {
+                        case 1:
+                            agregarEvaluacion(empleado->pilaEvaluaciones);
+                            break;
+                        case 2:
+                            mostrarEvaluacionesEmpleado(empleado->pilaEvaluaciones);
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            gotoxy(x, 12);
+                            cout << "Opción inválida.\n";
+                            system("pause");
+                            break;
+                    }
+                } while (opcionEvaluacion != 3);
+                break;
+            }
+            case 3:
+                gotoxy(x, 10);
+                cout << "Saliendo del programa...\n";
+                system("pause");
+                break;
+            default:
+                gotoxy(x, 10);
+                cout << "Opción inválida. Intente de nuevo.\n";
+                system("pause");
+                break;
+        }
+    } while (opcionPrincipal != 3);
+
+
+    Empleado* temp;
+    while (listaEmpleados != nullptr) {
+        temp = listaEmpleados;
+        listaEmpleados = listaEmpleados->siguiente;
+
+        // Liberar la pila de evaluaciones
+        if (temp->pilaEvaluaciones != nullptr) {
+            Evaluacion* evalTemp;
+            while (temp->pilaEvaluaciones->cima != nullptr) {
+                evalTemp = temp->pilaEvaluaciones->cima;
+                temp->pilaEvaluaciones->cima = temp->pilaEvaluaciones->cima->siguiente;
+                delete evalTemp;
+            }
+            delete temp->pilaEvaluaciones;
+        }
+
+        delete temp;
+    }
+
+    return 0;
 }
